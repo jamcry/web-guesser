0f8c6beb8e59e18da537d3f0decdb9337d7a38e5
jamcry <jamcry@hotmail.com>
2019-04-19 18:42:33 +0300

    Complete game with colorful button

--- web_guesser.rb
+++ web_guesser.rb
@@ -7,23 +7,23 @@ def check_guess(guess)
   return if guess.nil?
   guess = guess.to_i
   if guess > SECRET_NUMBER + 5
-    "Way too high!"
+    ["Way too high!", "red",]
   elsif guess > SECRET_NUMBER
-    "Too high!"
+    ["Too high!", "orange"]
   elsif guess < SECRET_NUMBER
-    "Too low! #{guess}"
+    ["Too low!", "orange"]
   elsif guess < SECRET_NUMBER - 5
-    "Way too low"
+    ["Way too low", "red"]
   elsif guess == SECRET_NUMBER
-    "You got it right!\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+    ["You got it right!\nThe SECRET NUMBER is #{SECRET_NUMBER}", "green"]
   end
 end
 
 get '/' do
   # get the user guess
   user_guess = params['guess']
-  message = check_guess(user_guess)
+  message, button_color = check_guess(user_guess)
   # pass local variables 'SECRET_NUMBER' and 'message'
   # to ERB template named 'index'
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :button_color => button_color}
 end
