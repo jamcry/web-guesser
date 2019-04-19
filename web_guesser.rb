b0e479f74efa643b2439444a59c946b083d547c3
jamcry <jamcry@hotmail.com>
2019-04-19 18:28:21 +0300

    Complete the game

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,29 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-random_number = rand(101)
+SECRET_NUMBER = rand(101)
+
+def check_guess(guess)
+  return if guess.nil?
+  guess = guess.to_i
+  if guess > SECRET_NUMBER + 5
+    "Way too high!"
+  elsif guess > SECRET_NUMBER
+    "Too high!"
+  elsif guess < SECRET_NUMBER
+    "Too low! #{guess}"
+  elsif guess < SECRET_NUMBER - 5
+    "Way too low"
+  elsif guess == SECRET_NUMBER
+    "You got it right!\nThe SECRET NUMBER is #{SECRET_NUMBER}"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => random_number}
+  # get the user guess
+  user_guess = params['guess']
+  message = check_guess(user_guess)
+  # pass local variables 'SECRET_NUMBER' and 'message'
+  # to ERB template named 'index'
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
 end
